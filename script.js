a3b97e13133286e2b295f99c3065dde53319340a
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-09-15 14:33:12 -0600

    table now generated from JS, added styling to table

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const tableBody = document.querySelector("tbody");
 const currentLibrary = [];
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 255, true);
+const soiaf = new Book("Game of Thrones", "G.R.R. Martin", 835, false);
 
 function Book(title, author, numPages, isRead) {
     this.title = title;
@@ -15,10 +18,34 @@ function addBookToLibrary(tome) {
     currentLibrary.push(tome);
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 255, true);
-const soiaf = new Book("Game of Thrones", "G.R.R. Martin", 835, false);
+function displayBooks() {
+    currentLibrary.forEach((tome) => {
+        const tr = document.createElement("tr");
+        let td = document.createElement("td")
+        
+        let tableData = document.createTextNode(`${tome.title}`);
+        td.appendChild(tableData);
+        tr.appendChild(td);
+        td = document.createElement("td");
+        tableData = document.createTextNode(`${tome.author}`);
+        td.appendChild(tableData);
+        tr.appendChild(td);
+        td = document.createElement("td");
+        tableData = document.createTextNode(`${tome.numPages}`);
+        td.setAttribute("class", "aligned-right");
+        td.appendChild(tableData);
+        tr.appendChild(td);
+        td = document.createElement("td");
+        tableData = document.createTextNode(`${tome.isRead}`);
+        td.setAttribute("class", "aligned-center");
+        td.appendChild(tableData);
+        tr.appendChild(td);
+
+        tableBody.append(tr);
+    });
+}
 
 addBookToLibrary(theHobbit);
 addBookToLibrary(soiaf);
+displayBooks();
 
-console.log(currentLibrary);
